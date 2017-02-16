199a120c308e4c0f406daa3c5f8434063af69824
Thibault Notargiacomo <gnthibault@gmail.com>
2017-02-16 15:01:52 +0100

    cmake now builds code+test but not where we want

--- test/cmake/libtest.cmake
+++ test/cmake/libtest.cmake
@@ -1,13 +1,11 @@
-
 macro(add_cuda_lib_executable exename)
-  set(dependencyName ${ARGN})
   cuda_add_executable(${exename} "${exename}.cu")
-  target_link_libraries(${exename} ${dependencyName} )
-endmacro()
-
-macroo(add_lib_executable exename)
   set(dependencyName ${ARGN})
-  add_executable(${exename} "${exename}.cpp")
   target_link_libraries(${exename} ${dependencyName} )
 endmacro()
 
+macro(add_lib_executable exename)
+  add_executable(${exename} "${exename}.cpp")
+  set(dependencyName ${ARGN})
+  target_link_libraries(${exename} ${dependencyName} )
+endmacro()
---
+++ test/src/cudalibtest1.cu
@@ -0,0 +1,7 @@
+#include <lib.h>
+
+int main(int argc, char* argv[]) {
+  LibObj l;
+  l.libCall();
+  return EXIT_SUCCESS;
+}
