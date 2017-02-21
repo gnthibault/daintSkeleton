e4971e79f92885c668b29037ce612c5e11a1b4ac
Thibault Notargiacomo <gnthibault@gmail.com>
2017-02-21 10:51:51 +0100

    updated script to launch on 4 nodes

--- scripts/launchApp1.sh
+++ scripts/launchApp1.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="MyProject"
 #SBATCH --time=00:05:00
 #SBATCH --constraint=gpu
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=debug
 #SBATCH --output=app1.%j.o
--- scripts/launchApp1WithProfiling.sh
+++ scripts/launchApp1WithProfiling.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="MyProjectWithProfiling"
 #SBATCH --time=00:05:00
 #SBATCH --constraint=gpu
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=debug
 
