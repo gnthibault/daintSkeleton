29bccba927e4f2faa4424dfcffb6f972385ea54a
Thibault Notargiacomo <gnthibault@gmail.com>
2017-08-23 16:18:29 +0200

    Now using the gpu target for launching all examples

--- scripts/launchApp1.sh
+++ scripts/launchApp1.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name="MyProject"
 #SBATCH --time=00:05:00
-#SBATCH --constraint=mc
+#SBATCH --constraint=gpu
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=debug
