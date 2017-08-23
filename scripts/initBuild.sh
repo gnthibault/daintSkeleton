657abf030219be788d2386cdbfc5f71aa48974dc
Thibault Notargiacomo <gnthibault@gmail.com>
2017-08-23 15:49:07 +0200

    First load daint-gpu module

--- scripts/initBuild.sh
+++ scripts/initBuild.sh
@@ -1,6 +1,6 @@
 export CRAYPE_LINK_TYPE=dynamic
+module load daint-gpu
 module swap PrgEnv-cray/6.0.3 PrgEnv-gnu
 module load cudatoolkit
-module load daint-gpu
 module load CMake/3.6.2
 module load Boost/1.63.0-CrayGNU-2016.11-Python-3.5.2
