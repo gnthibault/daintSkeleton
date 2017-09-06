c4e0a95c3c8d6381f62a64f16fe4c78abc6313b1
Thibault Notargiacomo <gnthibault@gmail.com>
2017-09-06 09:51:19 +0200

    Updated cmake in initscript

--- scripts/initBuild.sh
+++ scripts/initBuild.sh
@@ -2,5 +2,5 @@ export CRAYPE_LINK_TYPE=dynamic
 module load daint-gpu
 module swap PrgEnv-cray/6.0.3 PrgEnv-gnu
 module load cudatoolkit
-module load CMake/3.6.2
+module load CMake/3.8.1
 module load Boost/1.63.0-CrayGNU-2016.11-Python-3.5.2
